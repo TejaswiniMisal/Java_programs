717e1046a8eb47c2f20537d8b4a7374f85c7d098
Tejaswini Misal <163717058+TejaswiniMisal@users.noreply.github.com>
2025-03-01 22:05:54 +0530

    Add files via upload

---
+++ CourseName.java
@@ -0,0 +1,13 @@
+class CourseName {
+    // No-argument constructor
+    public CourseName() {
+        System.out.println("Course Name: Default Course");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Creating an object using the no-argument constructor
+        new CourseName(); // Displays "Course Name: Default Course"
+    }
+}
