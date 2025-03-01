aa405af0e8421e729f909a8398a600d12ac3795d
Tejaswini Misal <163717058+TejaswiniMisal@users.noreply.github.com>
2025-03-01 22:04:30 +0530

    Add files via upload

---
+++ evenodddowhile.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class evenodddowhile{
+	
+	public static void main (String Args[]){
+           int i=1;
+           do
+              { 
+  		if(i%2==0)
+		{
+                    System.out.println("it is even" +i);
+                }
+                else 
+		{
+         	    System.out.println("it is odd" +i);
+          	}
+                i++;
+                
+              }
+     		while(i<=100);  
+	}         
+}
+
+           	
